6b542bbce56e90a07b6cf5e99d5f87bcbf1dfca9
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-06-20 00:25:57 +0530

    Initial portfolio commit

---
+++ js/portfolios.js
@@ -0,0 +1,22 @@
+var portfolio_data = [
+  {
+    id: "aspireai",
+    images: [
+      {
+        src: "./images/aspireai-images/1.png",
+        thumb: "./images/aspireai-images/1.png",
+        subHtml: "<h4>AspireAI</h4><p>Pre-Final Year Project</p>",
+      },
+    ],
+  },
+  {
+    id: "zudosblog",
+    images: [
+      {
+        src: "./images/zudosblog-images/1.png",
+        thumb: "./images/zudosblog-images/1.png",
+        subHtml: "<h4>Zudo's Blog</h4><p>Blogging Website</p>",
+      },
+    ],
+  },
+];
